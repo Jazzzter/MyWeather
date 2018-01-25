d1511e511079cebcd8df526e4332745a55f92d7f
Jazzzter <zadorozhniy.igor@gmail.com>
2018-01-25 13:18:23 +0300

    in progress

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
-var humidity, pressure, temperature, windSpeed, weatherSummary, object;
+var timezone, humidity, pressure, temperature, windSpeed, object, weatherSummery;
 
-var element = function(id) {
-  document.getElementById(id);
-}
+var element = function (id) {
+    return document.getElementById(id);
+};
 
-window.onload = function() {
-    humidity = element("current-humidity");
-    pressure = element("current-pressure");
-    temperature = element("current-temperature");
-    windSpeed = element("current-wind-speed");
-    weatherSummary = element("weather-summary");
+window.onload = function () {
+    timezone = element('timezone');
+    humidity = element('current-humidity');
+    pressure = element('current-pressure');
+    temperature = element('current-temperature');
+    windSpeed = element('current-wind-speed');
+    weatherSummery = element('weather-summary');
 };
 
 function getWeather() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var lat = position.coords.latitude;
-          var long = position.coords.longitude;
-          showWeather(lat, long);
-        })
-    }
-    else {
-        return window.alert("Could not get location");
-    }
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+            showWeather(lat, long);
+        });}
 }
 
 function showWeather(lat, long) {
-    var url = `https://api.darksky.net/forecast/f672ff13193bfcc40427a678ebfdbc71/${lat},${long}` + `?format=jsonp&callback=displayWeather`;
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
-    displayWeather();
+  var url = `https://api.darksky.net/forecast/f672ff13193bfcc40427a678ebfdbc71/${lat},${long}` + `?format=jsonp&callback=displayWeather`;
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function displayWeather(object) {
-    humidity.innerHTML = "Humidity: " + humidityPercentage(object.currently.humidity) + "%";
-    pressure.innerHTML = "Pressure: " + object.currently.pressure + " mb";
-    temperature.innerHTML = "Temperature: " + farenheitToCelsius(object.currently.temperature) + " C" + " / " + object.currently.temperature + " F";
-    windSpeed.innerHTML = "Wind Speed: " + knotsToKilometres(object.currently.windSpeed) + " km/h";
-    weatherSummary.innerHTML = "Current Location: " + object.timezone + " <br/> <br/> Weather Summary: " + object.currently.summary;
-    document.getElementById("weather-summary").style.backgroundColor = "hsl(216, 100%, 60%)";
-    console.log(object);
+  timezone.innerHTML = '' + object.timezone;
+  humidity.innerHTML = 'Humidity: ' + humidityPercentage(object.currently.humidity) + '%';
+  pressure.innerHTML = 'Pressure: ' + object.currently.pressure + 'mb';
+  temperature.innerHTML = 'Temperature: ' + farenheitToCelsium(object.currently.temperature) + 'C' + '/' + object.currently.temperature;
+  windSpeed.innerHTML = 'Wind Speed: ' + knotsToKilometers(object.currently.windSpeed) + 'km/h';
+  weatherSummery.innerHTML = '' + object.minutely.summary;
+  
+  document.getElementById('weather-summary').style.backgroundColor = "darkblue";
 }
 
-function farenheitToCelsius(k) {
-    return Math.round((k - 32) * 0.5556 );
+function humidityPercentage(k) {
+  return Math.round(k * 100);
 }
 
-function humidityPercentage(h) {
-    return Math.round(h * 100);
+function farenheitToCelsium(c) {
+  return Math.round((c - 32) * 0.556);
 }
 
-function knotsToKilometres(knot) {
-    return Math.round(knot * 1.852);
+function knotsToKilometers(kn) {
+  return Math.round(kn * 1.852);
 }
