06c7eda3871c6b92778a42d800328027639ceb4f
Jazzzter <zadorozhniy.igor@gmail.com>
2018-01-25 11:18:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var humidity, pressure, temperature, windSpeed, weatherSummary, object;
+
+var element = function(id) {
+  document.getElementById(id);
+}
+
+window.onload = function() {
+    humidity = element("current-humidity");
+    pressure = element("current-pressure");
+    temperature = element("current-temperature");
+    windSpeed = element("current-wind-speed");
+    weatherSummary = element("weather-summary");
+};
+
+function getWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var lat = position.coords.latitude;
+          var long = position.coords.longitude;
+          showWeather(lat, long);
+        })
+    }
+    else {
+        return window.alert("Could not get location");
+    }
+}
+
+function showWeather(lat, long) {
+    var url = `https://api.darksky.net/forecast/f672ff13193bfcc40427a678ebfdbc71/${lat},${long}` + `?format=jsonp&callback=displayWeather`;
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    displayWeather();
+}
+
+function displayWeather(object) {
+    humidity.innerHTML = "Humidity: " + humidityPercentage(object.currently.humidity) + "%";
+    pressure.innerHTML = "Pressure: " + object.currently.pressure + " mb";
+    temperature.innerHTML = "Temperature: " + farenheitToCelsius(object.currently.temperature) + " C" + " / " + object.currently.temperature + " F";
+    windSpeed.innerHTML = "Wind Speed: " + knotsToKilometres(object.currently.windSpeed) + " km/h";
+    weatherSummary.innerHTML = "Current Location: " + object.timezone + " <br/> <br/> Weather Summary: " + object.currently.summary;
+    document.getElementById("weather-summary").style.backgroundColor = "hsl(216, 100%, 60%)";
+    console.log(object);
+}
+
+function farenheitToCelsius(k) {
+    return Math.round((k - 32) * 0.5556 );
+}
+
+function humidityPercentage(h) {
+    return Math.round(h * 100);
+}
+
+function knotsToKilometres(knot) {
+    return Math.round(knot * 1.852);
+}
